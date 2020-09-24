08a6d3b101711035da8584350a8116beb15b12a9
長町俊吾 <nagamaaaaachi@gmail.com>
2020-09-24 22:21:24 +0900

    rename About to Trends

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,26 +1,23 @@
+import Home from "@/views/Home.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/trends",
+    name: "Trends",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "@/views/Trends.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 });
 
 export default router;
